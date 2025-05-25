c23bd640ac9432fc3b8b09c8e1e470e020f1ce33
mark3245 <144309065+mark3245@users.noreply.github.com>
2025-05-25 13:39:50 +0700

    Delete client directory

--- client/scenes/ShopScene.js
+++
@@ -1,9 +0,0 @@
-export default class ShopScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'ShopScene' });
-  }
-
-  create() {
-    // Отображение доступных товаров и обработка покупок
-  }
-}
--- client/scenes/UIScene.js
+++
@@ -1,9 +0,0 @@
-export default class UIScene extends Phaser.Scene {
-  constructor() {
-    super({ key: 'UIScene' });
-  }
-
-  create() {
-    // Отображение счёта и других элементов интерфейса
-  }
-}
